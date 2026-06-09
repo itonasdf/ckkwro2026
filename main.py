b12c18c5374ccd3ac639d00c87313f76b9f0612f
itonasdf <forclassroomonly14@gmail.com>
2026-06-09 22:37:50 +0700

    mosaic logic

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.hubs import PrimeHub
 
 from huskylens import Huskylens, Block, ALGORITHM_COLOR_RECOGNITION
-from drivebase import DriveBaseFramework, MissionMotor, LEFT, RIGHT
+from drivebase import DriveBaseFramework, MissionMotor, PIVOT_LEFT, PIVOT_RIGHT
 
 prime_hub = PrimeHub()
 husky = Huskylens(Port.E)
@@ -40,33 +40,16 @@ w = DriveBaseFramework(
     },
 )
 
-def solve_mosaic(tiles: list[Block]) -> list[list[int]]:
-    filtered = [tile for tile in tiles if abs(tile.ratio - 1.0) <= 0.25 and tile.area <= 2000]
+def solve_mosaic(tiles: list[Block], ratio_tolerance: int, area_tolerance: int) -> list[list[int]] | None:
+    filtered = [tile for tile in tiles if abs(tile.ratio() - ratio_tolerance) <= 1.0 and tile.area() <= area_tolerance]
+    if len(filtered) < 12: return None
     sort_by_row = sorted(filtered, key = lambda tile: tile.y)
-    length = len(filtered)
-
-    #return [[v.id for v in sorted(sort_by_row[i:i+4], key = lambda tile: tile.x)] for i in range(0, 12, 4)]
-
-    result = []
-    for i in range(3):
-        temp = []
-        for j in range(4):
-            idx = i * 4 + j
-            if idx < length:
-                tile = sort_by_row[idx]
-                temp.append((tile.id, tile.x))
-            else: temp.append((1, 9999))
-        result.append([t[0] for t in sorted(temp, key = lambda t: t[1])])
-
-    return result
-
+    return [[v.id for v in sorted(sort_by_row[i:i+4], key = lambda tile: tile.x)] for i in range(0, 12, 4)]
 
 
 def main():
     # write your program here
-
     return 0
 
 
-
-if __name__ == "__main__": main()
+main()
