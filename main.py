538ec344b2996d8d2fb9d9fc3867638d29326420
itonasdf <forclassroomonly14@gmail.com>
2026-06-22 23:56:32 +0700

    testing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Program's Entry Point for WRO2026 Senior
+Program's Entry Point for WRO2026 Senior (BETA)
 """
 
 from pybricks.parameters import Port, Color, Direction
@@ -7,27 +7,25 @@ from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.hubs import PrimeHub
 
 from huskylens import Huskylens, Block, ALGORITHM_COLOR_RECOGNITION
-from drivebase import DriveBaseAPI, MissionMotor, PIVOT_LEFT, PIVOT_RIGHT
+from beta_drivebase import DriveBaseExtended, MissionMotor, PIVOT_LEFT, PIVOT_RIGHT
 
 prime_hub = PrimeHub()
 #husky = Huskylens(Port.E)
 #m1 = MissionMotor(Motor(Port.C))
 #m2 = MissionMotor(Motor(Port.D))
-w = DriveBaseAPI(
+w = DriveBaseExtended(
     Motor(Port.A, Direction.CLOCKWISE), 
     Motor(Port.B, Direction.COUNTERCLOCKWISE), 
     ColorSensor(Port.F), 
     hub = prime_hub,
     straight_params = {
-        10:  (0.0, 0.0, 0.0), -10:  (0.0, 0.0, 0.0),
-        25:  (0.0, 0.0, 0.0), -25:  (0.0, 0.0, 0.0),
+        30:  (0.0, 0.0, 0.0), -30:  (0.0, 0.0, 0.0),
         50:  (0.0, 0.0, 0.0), -50:  (0.0, 0.0, 0.0),
         75:  (0.0, 0.0, 0.0), -75:  (0.0, 0.0, 0.0),
         100: (0.0, 0.0, 0.0), -100: (0.0, 0.0, 0.0),
     },
     tagline_params = {
-        10:  (0.0, 0.0, 0.0), -10:  (0.0, 0.0, 0.0),
-        25:  (0.0, 0.0, 0.0), -25:  (0.0, 0.0, 0.0),
+        30:  (0.0, 0.0, 0.0), -30:  (0.0, 0.0, 0.0),
         50:  (0.0, 0.0, 0.0), -50:  (0.0, 0.0, 0.0),
         75:  (0.0, 0.0, 0.0), -75:  (0.0, 0.0, 0.0),
         100: (0.0, 0.0, 0.0), -100: (0.0, 0.0, 0.0),
@@ -48,6 +46,5 @@ def getMosaicData(tiles: list[Block], ratio_tolerance: int, area_tolerance: int)
 
 def main():
     return 0
-
 
 main()
